5ff6e99a77dfb7351d205b0ac1cc59c3563373d8
arthurvergacas <arthurvdm@gmail.com>
2020-07-03 21:29:48 -0300

    implemented alpha beta pruning optmization

--- ai.py
+++ ai.py
@@ -5,12 +5,15 @@ class Ai:
     def __init__(self):
 
         self.manager = GameManager()
+        self.counter = 0
 
-    def minimax(self, board, OPlayer, depth=float('inf')):
+    def minimax(self, board, OPlayer, alpha, beta, depth=float('inf')):
         # return the static evaluation of the final possible game
         # X wins: -1
         # O wins: +1
         # tie: 0
+
+        self.counter += 1
         if depth == 0 or self.manager.checkWin(board) or self.manager.checkTie(board):
             if self.manager.checkWin(board, True) == 'X':
                 return -1
@@ -26,9 +29,14 @@ class Ai:
             for cell in board:
                 if cell.state == '':
                     cell.state = 'O'
-                    value = self.minimax(board, False, depth=depth - 1)
+                    value = self.minimax(
+                        board, False, alpha, beta, depth=depth - 1)
                     cell.state = ''
                     maxValue = max(maxValue, value)
+                    alpha = max(alpha, value)
+                    if beta <= alpha:
+                        break
+
             return maxValue
 
         if OPlayer == False:
@@ -36,9 +44,13 @@ class Ai:
             for cell in board:
                 if cell.state == '':
                     cell.state = 'X'
-                    value = self.minimax(board, True, depth=depth - 1)
+                    value = self.minimax(
+                        board, True, alpha, beta, depth=depth - 1)
                     cell.state = ''
                     minValue = min(minValue, value)
+                    beta = min(beta, value)
+                    if beta <= alpha:
+                        break
             return minValue
 
     def play(self, board):
@@ -51,11 +63,12 @@ class Ai:
         for cell in board:
             if cell.state == '':
                 cell.state = 'O'
-                score = self.minimax(board, False)
+                score = self.minimax(board, False, float('-inf'), float('inf'))
                 cell.state = ''
 
                 if score > bestScore:
                     bestScore = score
                     bestCell = cell
+        print(self.counter)
 
         bestCell.changeState('O')
